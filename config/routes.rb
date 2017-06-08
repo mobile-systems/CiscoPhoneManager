1121259a8d86aca906a71c0bb9389e9cbecd3ad1
Dud <000Dud000@gmail.com>
2017-06-08 21:20:37 +0300

    App::Routes Add main resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :address_books
+  resources :areas
+  resources :phone_numbers
+  resources :phone_number_groups
+  resources :phone_devices
+  resources :phone_users
+
+  resources :tech_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
